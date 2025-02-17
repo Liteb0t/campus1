707e5f50f7a7a825697c69779f5700139deab783
Alex Ward <c3032806@hallam.shu.ac.uk>
2025-02-17 12:58:02 +0000

    css added

--- master/views.py
+++ master/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    return HttpResponse("Hi guys this is the page that is not behind the security wall, go to /secure or something to test that")
+    return render(request, "templatetest.html")
 
 def templatetest(request):
     return render(request, "templatetest.html")
