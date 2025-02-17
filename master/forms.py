a50e2298a748da586a0b43a9f3fcdb3157b76e9e
Alex Ward <c3032806@hallam.shu.ac.uk>
2025-02-17 12:32:50 +0000

    css added

--- master/forms.py
+++ master/forms.py
@@ -2,5 +2,10 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'placeholder': 'username'}))
-    password = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
+    username = forms.CharField(label='username')
+    password = forms.CharField(label='password')
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'username'
+        self.fields['password'].widget.attrs['placeholder'] = 'password'
