053722c89f75a3af23165548a670a35f3de3e7b5
Liteb0t <mpandccfanclub@gmail.com>
2025-02-19 20:04:54 +0000

    Student Creation Form (MVP) + db_admin pagination

--- master/views.py
+++ master/views.py
@@ -27,9 +27,46 @@ def access_db_admin(request):
     submissions = Submission.objects.all()
     line_managers = LineManager.objects.all()
 
+    if request.GET.__contains__("Jobs_page"):
+        jobs_page_number = request.GET.get("Jobs_page")
+    else:
+        jobs_page_number = 1
+    jobs_paginator = Paginator(jobs, 20)
+    jobs_page_obj = jobs_paginator.get_page(jobs_page_number)
 
+    if request.GET.__contains__("Students_page"):
+        students_page_number = request.GET.get("Students_page")
+    else:
+        students_page_number = 1
+    students_paginator = Paginator(students, 20)
+    students_page_obj = students_paginator.get_page(students_page_number)
 
-    return render(request, "db_view/access_db_admin.html", {"Jobs": jobs, "Students": students, "Submissions": submissions, "LineManagers": line_managers})
+    if request.GET.__contains__("Submissions_page"):
+        submissions_page_number = request.GET.get("Submissions_page")
+    else:
+        submissions_page_number = 1
+    submissions_paginator = Paginator(submissions, 20)
+    submissions_page_obj = submissions_paginator.get_page(submissions_page_number)
+
+    if request.GET.__contains__("LineManagers_page"):
+        line_managers_page_number = request.GET.get("LineManagers_page")
+    else:
+        line_managers_page_number = 1
+    line_managers_paginator = Paginator(line_managers, 20)
+    line_managers_page_obj = line_managers_paginator.get_page(line_managers_page_number)
+
+    message = None
+    student_creation_form = StudentCreationForm()
+    if request.method == "POST":
+        form = StudentCreationForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            message = "Added student!"
+        else:
+            message = "Form invalid, Student not added!"
+
+    return render(request, "db_view/access_db_admin.html", {"Jobs": jobs_page_obj, "Students": students_page_obj, "Submissions": submissions_page_obj, "LineManagers": line_managers_page_obj, "StudentCreationForm": student_creation_form, "Message": message})
 
 def access_db_student(request):
     submissions = Submission.objects.all()
@@ -38,12 +75,11 @@ def access_db_student(request):
         if request.GET.__contains__(search_parameter):
             # submissions = submissions.filter(search_parameter=request.GET[search_parameter])
             submissions = submissions.filter(**{search_parameter: request.GET[search_parameter]})
-    if request.GET.__contains__("page"):
-        page_number = request.GET.get("page")
+    if request.GET.__contains__("Submissions_page"):
+        submissions_page_number = request.GET.get("Submissions_page")
     else:
-        page_number = 1
-    paginator = Paginator(submissions, 20)
-    page_obj = paginator.get_page(page_number)
-    student_creation_form = StudentCreationForm()
-    return render(request, "db_view/access_db_student.html", {"Submissions": page_obj, "ValidSearchParameters": valid_search_parameters, "StudentCreationForm": student_creation_form})
+        submissions_page_number = 1
+    submissions_paginator = Paginator(submissions, 20)
+    submissions_page_obj = submissions_paginator.get_page(submissions_page_number)
+    return render(request, "db_view/access_db_student.html", {"Submissions": submissions_page_obj, "ValidSearchParameters": valid_search_parameters})
 
