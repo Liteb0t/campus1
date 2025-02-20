c5ccb540bf774bac6f9430c524aa58d7d3041f52
RyanJames100 <ryanljames64@gmail.com>
2025-02-20 14:56:44 +0000

    fied urls in line with potential client needs

--- master/urls.py
+++ master/urls.py
@@ -3,14 +3,13 @@ from . import views
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
-    # path("accounts/logged_out", views.logged_out, name="logged_out"),
     path("", views.homepage, name="homepage"),
     path("secure", views.secure, name="secure"),
     path("profile", views.profile, name="profile"),
     path("recruiter_profile", views.recruiter_profile, name="recruiter_profile"),
     path("admin_profile", views.admin_profile, name="admin_profile"),
-    path("access_db_admin", views.access_db_admin, name="access_db_admin"),
-    path("access_db_student", views.access_db_student, name="access_db_student"),
+    path("admin", views.access_db_admin, name="access_db_admin"),
+    path("student", views.access_db_student, name="access_db_student"),
     path("TempUpdatePageForTesting/<int:id>", views.updatestudent, name="update"),
 
 ]
