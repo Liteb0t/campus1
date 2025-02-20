103ca94c055b4d3503f115286080a3c5fdb02116
RyanJames100 <ryanljames64@gmail.com>
2025-02-20 12:40:08 +0000

    added new profile files for recruiter and admin; crud required.

--- master/urls.py
+++ master/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("secure", views.secure, name="secure"),
     path("profile", views.profile, name="profile"),
+    path("recruiter_profile", views.recruiter_profile, name="recruiter_profile"),
+    path("admin_profile", views.admin_profile, name="admin_profile"),
     path("access_db_admin", views.access_db_admin, name="access_db_admin"),
     path("access_db_student", views.access_db_student, name="access_db_student"),
 ]
