29461263de8e970f6810dc1f6529ed5140d06dbd
AlexWardSHU <c3032806@hallam.shu.ac.uk>
2025-02-20 10:40:05 +0000

    changes were made

--- master/forms.py
+++ master/forms.py
@@ -5,4 +5,10 @@ from master.models import Student
 class StudentCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Student
-        fields = UserCreationForm.Meta.fields + ("on_visa",)
+        fields = UserCreationForm.Meta.fields + ("on_visa", "first_name", "last_name", "email")
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None,
+            'email': None,
+        }
