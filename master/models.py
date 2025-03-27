e3bb1a3c39de70116e655681a3211c7bb67d9460
c3012184 <c3012184@hallam.shu.ac.uk>
2025-03-27 11:33:37 +0000

    adjusting models to remake superuser functionality; WIP

--- master/models.py
+++ master/models.py
@@ -10,6 +10,9 @@ class CampusUserManager(BaseUserManager):
 
         user = self.model(
             email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name
         )
 
         user.set_password(password)
